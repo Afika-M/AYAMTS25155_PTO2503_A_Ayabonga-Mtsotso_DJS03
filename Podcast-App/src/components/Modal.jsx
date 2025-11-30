14c12f7a14005825026e3791c859e8612b33d283
Ayabonga Mtsotso <ayabonga.afika.mtsotso@gmail.com>
2025-11-30 19:00:49 +0200

    Updates the modal component

--- Podcast-App/src/components/Modal.jsx
+++ Podcast-App/src/components/Modal.jsx
@@ -4,11 +4,11 @@ const Modal = ({ podcast, onClose }) => {
   if (!podcast) return null; // don't render if nothing is selected
 
   return (
-    <div className="modal-overlay">
+    <div className="modal">
       <div className="modal-content">
         <button className="close-btn" onClick={onClose}>×</button>
         <h2>{podcast.title}</h2>
-        <img src={podcast.image} alt={podcast.title} />
+        <img src={podcast.image} className="modal-img"/>
         <p>Seasons: {podcast.seasons}</p>
         <p>Updated: {podcast.updated}</p>
         <p>Genres: {podcast.genres.join(", ")}</p>
